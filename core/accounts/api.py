ec09aeddd01e4587f922ecd4ca04b43dd30e81d4
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2022-05-26 18:06:32 -0500

    Demo inicial

---
+++ core/accounts/api.py
@@ -0,0 +1,45 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
+from .serializers import UserSerializer, RegisterSerializer
+
+
+from django.contrib.auth import login
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = AuthToken.objects.create(user)
+        return Response({
+            "users": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": token[1]
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+
+
+# DEMO
+class MainUser(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-
-from . import views
+from django.urls import path, include
+from .api import RegisterAPI, LoginAPI, MainUser
+from knox import views as knox_views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('api/auth/register', RegisterAPI.as_view(), name="register" ),
+    path('api/auth/login', LoginAPI.as_view(), name="login"),
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name="logout"),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    #DEMO
+    path('api/auth/user', MainUser.as_view(), name="user"),
 ]
