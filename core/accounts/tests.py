a40432f949fa483bd37de40da5d239a8b97424c7
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2022-05-27 14:38:59 -0500

    Refactor test

--- core/accounts/tests.py
+++ core/accounts/tests.py
@@ -11,52 +11,51 @@ import base64
 
 
 class AccountTests(APITestCase):
+
+    register_url = reverse('register')
+    login_url = reverse('login')
+    user_url = reverse('user')
+
+    register_data = {
+        'username': 'TestUser',
+        'password': 'TestPassword',
+        'email': 'test@test.com'
+    }
+    bad_login_data = {
+        'username': 'TestUser',
+        'password': 'BadPassword'
+    }
+    good_login_data = {
+        'username': 'TestUser',
+        'password': 'TestPassword'
+    }
+
     def test_create_account(self):
         """
         Ensure we can create a new account object.
         """
-        register_url = reverse('register')
-        data = {
-            'username': 'DemoUser',
-            'password': 'S3cUR3P422W0rD',
-            'email': 'demo@gmail.com',
-        }
-        response = self.client.post(register_url, data, format='json')
+        response = self.client.post(self.register_url, self.register_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, 'DemoUser')
-        self.assertEqual(User.objects.get().email, 'demo@gmail.com')
+        self.assertEqual(User.objects.get().username, 'TestUser')
+        self.assertEqual(User.objects.get().email, 'test@test.com')
 
     def test_login_account(self):
         """
         Ensure we can create a new account object.
         """
-
-        register_url = reverse('register')
-        register_data = {
-            'username': 'TestUser',
-            'password': 'TestPassword',
-            'email': 'test@test.com'
-        }
-        response = self.client.post(register_url, register_data, format='json')
+        response = self.client.post(self.register_url, self.register_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, 'TestUser')
         self.assertEqual(User.objects.get().email, 'test@test.com')
 
         login_url = reverse('login')
-        bad_login_data = {
-            'username': 'TestUser',
-            'password': 'BadPassword'
-        }
-        response = self.client.post(login_url, bad_login_data, format='json')
+
+        response = self.client.post(login_url, self.bad_login_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        good_login_data = {
-            'username': 'TestUser',
-            'password': 'TestPassword'
-        }
-        response = self.client.post(login_url, good_login_data, format = 'json')
+        response = self.client.post(login_url, self.good_login_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Crear usuario en BD
@@ -70,35 +69,24 @@ class AccountTests(APITestCase):
         """
         Ensure we can logout of current session.
         """
-        register_url = reverse('register')
-        register_data = {
-            'username': 'TestUser',
-            'password': 'TestPassword',
-            'email': 'test@test.com'
-        }
-        response = self.client.post(register_url, register_data, format='json')
+        response = self.client.post(self.register_url, self.register_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, 'TestUser')
         self.assertEqual(User.objects.get().email, 'test@test.com')
 
-        login_url = reverse('login')
-        login_data = {
-            'username': 'TestUser',
-            'password': 'TestPassword'
-        }
-        response = self.client.post(login_url, login_data, format = 'json')
-        response = JsonResponse(str(response))
+
+        response = self.client.post(self.login_url, self.good_login_data, format='json')
+        response = JsonResponse(response.data)
         token = str(response.content[54:118])[2:-1]
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         db_token = AuthToken.objects.all()[1].token_key
         self.assertEqual(token[:8], db_token)
 
         logout_url = reverse('logout')
-        AUTH_TOKEN = "Token " + db_token
-        self.client.credentials(HTTP_AUTHORIZATION = AUTH_TOKEN)
+        auth_token = "Token " + db_token
+        self.client.credentials(HTTP_AUTHORIZATION=auth_token)
 
-        
         # Crear usuario en BD
         # Usar endpoint para logear usuario
         # Assert token generado en BD
@@ -121,35 +109,23 @@ class AccountTests(APITestCase):
         """
         Ensure the created token can be used to access-session restricted resources.
         """
-        user_url = reverse('user')
 
-        response = self.client.get(user_url)
+        response = self.client.get(self.user_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-        register_url = reverse('register')
-        register_data = {
-            'username': 'TestUser',
-            'password': 'TestPassword',
-            'email': 'test@test.com'
-        }
-        response = self.client.post(register_url, register_data, format='json')
+        response = self.client.post(self.register_url, self.register_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, 'TestUser')
         self.assertEqual(User.objects.get().email, 'test@test.com')
-        
-        login_url = reverse('login')
-        login_data = {
-            'username': 'TestUser',
-            'password': 'TestPassword'
-        }
-        response = self.client.post(login_url, login_data, format = 'json')
+
+        response = self.client.post(self.login_url, self.good_login_data, format='json')
         token = str(response.content[54:118])[2:-1]
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        AUTH_TOKEN = "Token " + token
-        self.client.credentials(HTTP_AUTHORIZATION = AUTH_TOKEN)
-        response = self.client.get(user_url)
+        auth_token = "Token " + token
+        self.client.credentials(HTTP_AUTHORIZATION=auth_token)
+        response = self.client.get(self.user_url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -160,4 +136,3 @@ class AccountTests(APITestCase):
         # Usar Token de login para acceder a endpoint de user
         # Assert respuesta de exito
         pass
-
