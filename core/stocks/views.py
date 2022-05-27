f864a12418a66b89f6cdb47f872d1d51b05c8830
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-05-27 15:54:39 -0500

    register endpoint development

--- core/stocks/views.py
+++ core/stocks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic,View
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Demo: Stonks - Stocks App")
+
+class IndexView(View):
+    template_name = "stocks/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request,self.template_name)
