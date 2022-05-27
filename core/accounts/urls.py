a46b31349803c93805f8c19b6c95eea081ad9dd0
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2022-05-26 22:27:38 -0500

    urls update

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('api/auth/register', RegisterAPI.as_view(), name="register" ),
     path('api/auth/login', LoginAPI.as_view(), name="login"),
     path('api/auth/logout', knox_views.LogoutView.as_view(), name="logout"),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/auth/logoutall', knox_views.LogoutAllView.as_view(), name='logoutall'),
     #DEMO
     path('api/auth/user', MainUser.as_view(), name="user"),
 ]
