130d117f5388e361d63460039ac7856ce67fd16b
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2022-05-26 20:16:10 -0500

    Primer test

--- core/accounts/tests.py
+++ core/accounts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        register_url = reverse('register')
+        data = {
+            'username': 'DemoUser',
+            'password': 'S3cUR3P422W0rD',
+            'email': 'demo@gmail.com',
+        }
+        response = self.client.post(register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'DemoUser')
+        self.assertEqual(User.objects.get().email, 'demo@gmail.com')
+
+    def test_login_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        pass
+
+    def test_token_validation(self):
+        """
+        Ensure the created token can be used to access-session restricted resources.
+        """
+        pass
--- core/core/settings.py
+++ core/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'NAME': 'db_stonks',
+        'USER': 'postgres',
         'OPTIONS': {
-            'service': 'db_service',
+            # Service File - Incompatible with Testing
+            # 'service': 'db_service',
+            # Password File - Must contain credentials for NAME, test_NAME and postgres databases
             'passfile': '.pgpass',
         },
     }
