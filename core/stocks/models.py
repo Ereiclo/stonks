7d4da9337611ea88c52866245672026142ee00ca
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-05-30 20:19:49 -0500

    nuevos modelos stocks

--- core/stocks/models.py
+++ core/stocks/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from accounts.models import Client
 
-# Create your models here.
+
+class Company(models.Model):
+    ruc = models.CharField(max_length=11, primary_key=True)
+    acronym = models.CharField(max_length=12)
+    lastest_price = models.FloatField()
+    stocks_for_client = models.ManytoManyField(Client, through="Portfolio")
+
+
+class Portfolio(models.Model):
+    client_dni = models.ForeignKey(Client, on_delete=models.Cascade)
+    company_ruc = models.ForeignKey(Company, on_delete=models.Cascade)
+    quantity = models.IntegerField()
+
+
+class Order(models.Model):
+    client_dni = models.ForeignKey(Client, on_delete=models.Cascade)
+    company_ruc = models.ForeignKey(Company, on_delete=models.Cascade)
+
+
+quantity = models.IntegerField()
+price = models.FloatField()
+transaction_type = models.BooleanField()
+
+
+class IncompleteOrder(models.Model):
+    order_id = models.ForeignKey(Order)
+    status = models.CharField(max_length=1)
+
+
+class CompleteOrder(models.Model):
+    order_id = models.ForeignKey(Order)
+    price_per_stock = models.FloatField()
