ec7b3ac03b7a84312f8cbf481caf3a72d07fa994
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-05-30 20:31:49 -0500

    modelos finalizados

--- core/stocks/models.py
+++ core/stocks/models.py
@@ -6,30 +6,30 @@ class Company(models.Model):
     ruc = models.CharField(max_length=11, primary_key=True)
     acronym = models.CharField(max_length=12)
     lastest_price = models.FloatField()
-    stocks_for_client = models.ManytoManyField(Client, through="Portfolio")
+    stocks_for_client = models.ManyToManyField(Client, through="Portfolio")
 
 
 class Portfolio(models.Model):
-    client_dni = models.ForeignKey(Client, on_delete=models.Cascade)
-    company_ruc = models.ForeignKey(Company, on_delete=models.Cascade)
+    client_dni = models.ForeignKey(Client, on_delete=models.CASCADE)
+    company_ruc = models.ForeignKey(Company, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    class Meta:
+        unique_together = ('client_dni','company_ruc')
 
 
 class Order(models.Model):
-    client_dni = models.ForeignKey(Client, on_delete=models.Cascade)
-    company_ruc = models.ForeignKey(Company, on_delete=models.Cascade)
-
-
-quantity = models.IntegerField()
-price = models.FloatField()
-transaction_type = models.BooleanField()
+    client_dni = models.ForeignKey(Client, on_delete=models.CASCADE)
+    company_ruc = models.ForeignKey(Company, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    transaction_type = models.BooleanField()
 
 
 class IncompleteOrder(models.Model):
-    order_id = models.ForeignKey(Order)
+    order_id = models.ForeignKey(Order,on_delete = models.CASCADE)
     status = models.CharField(max_length=1)
 
 
 class CompleteOrder(models.Model):
-    order_id = models.ForeignKey(Order)
+    order_id = models.ForeignKey(Order,on_delete = models.CASCADE)
     price_per_stock = models.FloatField()
