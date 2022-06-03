9124c12ab09da5424cd45a75ea06b11cd6db1b5a
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-06-03 17:32:12 -0500

    fetch api data

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -11,36 +11,33 @@ def index(request):
 
 class IndexView(APIView):
     template_name = "stocks/index.html"
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
-class LoginView(APIView):
+class LoginView(View):
     template_name = "stocks/login.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
 
-class RegisterView(APIView):
+class RegisterView(View):
     template_name = "stocks/register.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
 
-class AccountView(APIView):
+class AccountView(View):
     template_name = "stocks/account.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
-class UpdateView(APIView):
+class UpdateView(View):
     template_name = "stocks/update.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
