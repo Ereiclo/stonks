7442ae9b60fe95d4478f99f7e72176068bd1a2f0
panchomb <francisco.magot@utec.edu.pe>
2022-06-03 18:18:19 -0500

    portafolio fix

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -9,38 +9,34 @@ def index(request):
     return HttpResponse("Demo: Stonks - Stocks App")
 """
 
-class IndexView(APIView):
+class IndexView(View):
     template_name = "stocks/index.html"
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
-class LoginView(APIView):
+class LoginView(View):
     template_name = "stocks/login.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
 
-class RegisterView(APIView):
+class RegisterView(View):
     template_name = "stocks/register.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
 
-class AccountView(APIView):
+class AccountView(View):
     template_name = "stocks/account.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
 class UpdateView(APIView):
     template_name = "stocks/update.html"
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
