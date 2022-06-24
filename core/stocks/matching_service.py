212fb7b3297113ae553ff2be9ca72ec181d0e6b8
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-06-24 18:56:17 -0500

    matching service

--- core/stocks/matching_service.py
+++ core/stocks/matching_service.py
@@ -1,6 +1,9 @@
 from doctest import DocFileSuite
 from .models import Portfolio,IncompleteOrder,CompleteOrder,Order
 
+
+
+
 #compra
 def matching_service_buy(order,relevant_orders):
     buyer = order.client_dni
@@ -35,7 +38,6 @@ def matching_service_buy(order,relevant_orders):
         current_order.avg_price = current_total_sold / (current_order.quantity - current_order.quantity_left)
 
         seller_total_sold -= quantity_sold * price
-        seller_portfolio.avg_price = seller_total_sold / seller_portfolio.quantity
         buyer_total_sold += quantity_sold * price
         buyer_portfolio.avg_price = buyer_total_sold / buyer_portfolio.quantity
 
@@ -49,10 +51,17 @@ def matching_service_buy(order,relevant_orders):
             IncompleteOrder.objects.filter(pk = current_order.id).delete()
             CompleteOrder.objects.create(order_id=current_order)
         
+
+        if seller_portfolio.quantity != 0:
+            seller_portfolio.avg_price = seller_total_sold / seller_portfolio.quantity
+            seller_portfolio.save()
+        else:
+            seller_portfolio.delete()
+
         current_order.save()
-        seller_portfolio.save()
         seller.save()
         buyer_portfolio.save()
+        order.save()
 
         if order.quantity_left == 0:
             IncompleteOrder.objects.filter(pk = order.id).delete()
@@ -68,6 +77,9 @@ def matching_service_buy(order,relevant_orders):
     buyer.save()
     order.save()
 
+
+
+
 #venta
 def matching_service_sell(order,relevant_orders):
     seller = order.client_dni
@@ -82,6 +94,7 @@ def matching_service_sell(order,relevant_orders):
     
     for current_order in relevant_orders:  	
         buyer = current_order.client_dni
+        buyer_portfolio = Portfolio.objects.get(client_dni = buyer, company_ruc = current_order.company_ruc)
         quantity_sold = min(order.quantity_left, current_order.quantity_left)
         price = current_order.price
 
@@ -92,9 +105,19 @@ def matching_service_sell(order,relevant_orders):
         current_total_sold = (current_order.quantity - current_order.quantity_left) * current_order.avg_price
         current_total_sold += quantity_sold * price
 
+        seller_total_sold = seller_portfolio.quantity * seller_portfolio.avg_price
+        buyer_total_sold = buyer_portfolio.quantity * buyer_portfolio.avg_price
+
         current_order.quantity_left -= quantity_sold
+        seller_portfolio.quantity -= quantity_sold
+        buyer_portfolio.quantity += quantity_sold
         order.quantity_left -= quantity_sold
         current_order.avg_price = current_total_sold / (current_order.quantity - current_order.quantity_left)
+
+        seller_total_sold -= quantity_sold * price
+        
+        buyer_total_sold += quantity_sold * price
+        buyer_portfolio.avg_price = buyer_total_sold / buyer_portfolio.quantity
 
         buyer.money -= quantity_sold * price
         seller.money += quantity_sold * price
@@ -103,15 +126,24 @@ def matching_service_sell(order,relevant_orders):
         matched = True
     
         if current_order.quantity_left == 0:
-            IncompleteOrder.filter(pk = current_order.id).delete()
-            CompleteOrder.create(order_id = current_order.id)
+            IncompleteOrder.objects.filter(pk = current_order.id).delete()
+            CompleteOrder.objects.create(order_id=current_order)
         
+
+        if seller_portfolio.quantity != 0:
+            seller_portfolio.avg_price = seller_total_sold / seller_portfolio.quantity
+            seller_portfolio.save()
+        else:
+            seller_portfolio.delete()
+
         current_order.save()
-        buyer.save()
+        seller.save()
+        buyer_portfolio.save()
+        order.save()
 
         if order.quantity_left == 0:
-            IncompleteOrder.filter(pk = order.id).delete()
-            CompleteOrder.create(order_id = order.id)
+            IncompleteOrder.objects.filter(pk = order.id).delete()
+            CompleteOrder.objects.create(order_id=order)
             break
 
     if matched:
@@ -125,13 +157,13 @@ def matching_service_sell(order,relevant_orders):
 
 def matching_service(order):
       	#lock()
-    company_orders = Order.objects.filter(company_ruc = order.company_ruc_id).exclude(client_dni_id = order.client_dni_id, transaction_type__startswith = order.transaction_type[0])
+    company_orders = Order.objects.filter(company_ruc = order.company_ruc_id).exclude(client_dni_id = order.client_dni_id).exclude( transaction_type__startswith = order.transaction_type[0])
     if order.transaction_type[0] == 'B':  # compra 
         company_orders.filter(price__lte = order.price).order_by("price","date")
         matching_service_buy(order,company_orders)
     elif order.transaction_type[0] == 'S':	# venta
-        Order.objects.filter(price__gte = order.price).order_by("-price","date")
-        # matching_service_sell(order,company_orders)
+        company_orders.filter(price__gte = order.price).order_by("-price","date")
+        matching_service_sell(order,company_orders)
 #unlock()
 
 
