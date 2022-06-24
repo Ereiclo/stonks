aa7506e60a0de40c08977baf66eaa9df77aa8234
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-06-24 18:03:21 -0500

    matching sell service

--- core/stocks/matching_service.py
+++ core/stocks/matching_service.py
@@ -23,21 +23,15 @@ def matching_service_buy(order,relevant_orders):
             break
 
 
-        # print(quantity_sold)
-        # print(current_order.quantity_left,"\n\n\n\n")
-        # print(current_order.quantity,"\n\n\n\n")
-        # prev_quantity_sold = curre
+        
         current_total_sold = (current_order.quantity - current_order.quantity_left) * current_order.avg_price
         current_total_sold += quantity_sold * price
 
-        # print(current_total_sold,"\n\n\n\n")
-        # print(current_order.quantity_left,"\n\n\n\n")
-        # print(current_order.quantity,"\n\n\n\n")
+        
         current_order.quantity_left -= quantity_sold
         seller_portfolio.quantity -= quantity_sold
         buyer_portfolio.quantity += quantity_sold
         order.quantity_left -= quantity_sold
-        # print(current_order.quantity_left,"\n\n\n\n")
         current_order.avg_price = current_total_sold / (current_order.quantity - current_order.quantity_left)
 
 
@@ -72,53 +66,58 @@ def matching_service_buy(order,relevant_orders):
     order.save()
 
 #venta
-# def matching_service_sell(order,relevant_orders):
-# seller = order.client_dni
-# total_bought = 0
-# total_quantity = 0
-# matched = False
+def matching_service_sell(order,relevant_orders):
+    seller = order.client_dni
+    total_bought = 0
+    total_quantity = 0
+    matched = False
+
+    try:
+        seller_portfolio = Portfolio.objects.get(client_dni = seller, company_ruc = order.company_ruc)
+    except Portfolio.DoesNotExist:
+        seller_portfolio = Portfolio.objects.create(client_dni = seller,company_ruc=order.company_ruc,quantity= 0)
     
-# for current_order in relevant_orders:  	
-# buyer = current_order.client_dni
-# quantity_sold = min(order.quantity_left, current_order.quantity_left)
-# price = current_order.price
+    for current_order in relevant_orders:  	
+        buyer = current_order.client_dni
+        quantity_sold = min(order.quantity_left, current_order.quantity_left)
+        price = current_order.price
 
-# if buyer.money < quantity_sold * price
-# continue	
-# #break
+        if buyer.money < quantity_sold * price:
+            continue	
+            #break
 
-# current_total_sold = (current_order.quantity - current_order.quantity_left) * current_order.avg_price
-# current_total_sold += quantity_sold * price
+        current_total_sold = (current_order.quantity - current_order.quantity_left) * current_order.avg_price
+        current_total_sold += quantity_sold * price
 
-# current_order.quantity_left -= quantity_sold
-# order.quantity_left -= quantity_sold
-# current_order.avg_price = current_total_sold / (current_order.quantity - current_order.quantity_left)
+        current_order.quantity_left -= quantity_sold
+        order.quantity_left -= quantity_sold
+        current_order.avg_price = current_total_sold / (current_order.quantity - current_order.quantity_left)
 
-# buyer.money -= quantity_sold * price
-# seller.money += quantity_sold * price
-# total_bought += quantity_sold * price
-# total_quantity += quantity_sold
-# matched = True
+        buyer.money -= quantity_sold * price
+        seller.money += quantity_sold * price
+        total_bought += quantity_sold * price
+        total_quantity += quantity_sold
+        matched = True
     
-# if current_order.quantity_left == 0:
-#     IncompleteOrder.filter(pk = current_order.id).delete()
-#     CompleteOrder.create(order_id = current_order.id)
+        if current_order.quantity_left == 0:
+            IncompleteOrder.filter(pk = current_order.id).delete()
+            CompleteOrder.create(order_id = current_order.id)
         
-# current_order.save()
-# buyer.save()
+        current_order.save()
+        buyer.save()
 
-# if order.quantity_left == 0:
-#     IncompleteOrder.filter(pk = order.id).delete()
-#     CompleteOrder.create(order_id = order.id)
-#     break
+        if order.quantity_left == 0:
+            IncompleteOrder.filter(pk = order.id).delete()
+            CompleteOrder.create(order_id = order.id)
+            break
 
-# if matched:
-#     order.company_ruc.latest_price = price
-#     order.company_ruc.save()
+    if matched:
+        order.company_ruc.latest_price = price
+        order.company_ruc.save()
 
-# order.avg_price = total_bought / total_quantity
-# seller.save()
-# order.save()
+    order.avg_price = total_bought / total_quantity
+    seller.save()
+    order.save()
 
 
 def matching_service(order):
